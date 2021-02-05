0fbc08ea0889b59f73b4d82e112e77b5ad4180e1
Kate Khl <kate.khlystunova@yandex.ru>
2021-02-05 09:16:52 +0300

    SB start

---
+++ hw03/src/test/java/org/khl/hw03/hw03/Hw03ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.khl.hw03.hw03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
